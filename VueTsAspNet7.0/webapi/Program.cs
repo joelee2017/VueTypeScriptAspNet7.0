4ec7e46ff21c242cc407475815d1806df89d2a82
joelee2017 <33776374+joelee2017@users.noreply.github.com>
2023-08-11 15:25:50 +0800

    Add files via upload

---
+++ VueTsAspNet7.0/webapi/Program.cs
@@ -0,0 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var AllowMyFrontEnd = "AllowMyFrontEnd";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: AllowMyFrontEnd,
+    policy =>
+    {
+        policy.WithOrigins("http://localhost:8095").AllowAnyMethod().AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.UseCors(AllowMyFrontEnd);
+
+app.Run();
